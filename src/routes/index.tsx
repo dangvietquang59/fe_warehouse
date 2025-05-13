8e7c8f0207ab0acb49a671a8db87e19711f928f0
Dang Viet Quang <dvquang5902@gmail.com>
2025-05-13 15:51:42 +0700

    fix: route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,32 +1,34 @@
-import { lazy } from "react";
-import { createBrowserRouter } from "react-router-dom";
-import MainLayout from "@/layouts/MainLayout";
-import BlankLayout from "@/layouts/BlankLayout";
-import Login from "@/pages/Login";
-import Logup from "@/pages/Logup";
+import { lazy } from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import MainLayout from '@/layouts/MainLayout';
+import BlankLayout from '@/layouts/BlankLayout';
 
-const Home = lazy(() => import("@/pages/Home"));
-const Dashboard = lazy(() => import("@/pages/Dashboard"));
+const Home = lazy(() => import('@/pages/Home'));
+const Dashboard = lazy(() => import('@/pages/Dashboard'));
+const Login = lazy(() => import('@/pages/Login'));
+const Logup = lazy(() => import('@/pages/Logup'));
 
 export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayout />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: "dashboard", element: <Dashboard /> },
-    ],
-  },
-  {
-    path:'/auth',
-    element: <BlankLayout/>,
-    children:[
-      {
-        path:"sign-in", element:<Login/>,
-      },
-      {
-        path:"sign-up", element:<Logup/>
-      }
-    ]
-  }
+    {
+        path: '/',
+        element: <MainLayout />,
+        children: [
+            { index: true, element: <Home /> },
+            { path: 'dashboard', element: <Dashboard /> },
+        ],
+    },
+    {
+        path: '/auth',
+        element: <BlankLayout />,
+        children: [
+            {
+                path: 'sign-in',
+                element: <Login />,
+            },
+            {
+                path: 'sign-up',
+                element: <Logup />,
+            },
+        ],
+    },
 ]);
