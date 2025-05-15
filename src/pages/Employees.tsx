51133d74a0ced6b31cd23813689f4c5048645d33
Dang Viet Quang <dvquang5902@gmail.com>
2025-05-15 17:29:59 +0700

    feat: update user page

--- src/pages/Employees.tsx
+++ src/pages/Employees.tsx
@@ -1,13 +1,20 @@
 import { useUsers } from '@/queries/user-query';
 import { RoleType, UserType } from '@/types/user-type';
 import { useTranslationCustom } from '@/utils/hooks/useTranslationCustom';
-import { Button, Table, Tag } from 'antd';
+import { Button, Modal, Table, Tag } from 'antd';
 import { Plus } from 'lucide-react';
+import { useState } from 'react';
 
 function Employees() {
     const { data: users, isLoading } = useUsers();
     const { t } = useTranslationCustom();
-
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    };
+    const handleOpenModal = () => {
+        setIsModalOpen(true);
+    };
     const mappingRole: Record<string, string> = {
         admin: 'blue',
         warehouse_manager: 'green',
@@ -51,26 +58,45 @@ function Employees() {
         },
     ];
     return (
-        <div className="flex flex-col gap-[20px] bg-white p-[20px] rounded-[10px]">
-            <div className="flex justify-between items-center">
-                <h1 className="text-2xl font-bold">{t.user.listEmployee}</h1>
-                <Button
-                    type="primary"
-                    className="w-fit flex items-center gap-[10px] h-[40px]"
-                    color="default"
-                    variant="solid"
-                >
-                    <Plus />
-                    {t.user.addEmployee}
-                </Button>
+        <>
+            <div className="flex flex-col gap-[20px] bg-white p-[20px] rounded-[10px]">
+                <div className="flex justify-between items-center">
+                    <h1 className="text-2xl font-bold">{t.user.listEmployee}</h1>
+                    <Button
+                        type="primary"
+                        className="w-fit flex items-center gap-[10px] h-[40px]"
+                        color="default"
+                        variant="solid"
+                        onClick={handleOpenModal}
+                    >
+                        <Plus />
+                        {t.user.addEmployee}
+                    </Button>
+                </div>
+                <Table
+                    columns={columns}
+                    dataSource={users?.data}
+                    loading={isLoading}
+                    rowKey={record => record.id}
+                />
             </div>
-            <Table
-                columns={columns}
-                dataSource={users?.data}
-                loading={isLoading}
-                rowKey={record => record.id}
-            />
-        </div>
+            <Modal
+                title={t.user.addEmployee}
+                open={isModalOpen}
+                onCancel={handleCloseModal}
+                footer={null}
+                centered
+                width={1200}
+                height={1200}
+            >
+                <div className="flex items-center justify-center">
+                    <iframe
+                        src="https://fe-warehouse-chi.vercel.app/auth/sign-up"
+                        className="w-[1000px] h-[1000px]"
+                    />
+                </div>
+            </Modal>
+        </>
     );
 }
 
